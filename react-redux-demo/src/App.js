3945b6fdac6f4a711bf376094229047a9fef77d3
pradeep misra <pradeep.misra@kognitiv.com>
2020-05-02 15:00:12 +0530

    updated the welcome message

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -6,17 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Welcome to React Redux Demo !
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It's working.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
       </header>
     </div>
